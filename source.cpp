864fbec9a7af292644064bb6c2f5958be937c9cd
BadiiiX <59808543+BadiiiiX@users.noreply.github.com>
2021-12-09 07:47:53 +0100

    Update CreateGrille()

--- source.cpp
+++ source.cpp
@@ -10,46 +10,44 @@
 #include <random>
 #include <string>
 
-//Définitions:
 enum LIMITS {
     COLUMNS = 32,
-    LINES   = 32
+    LINES = 32
 };
 
 enum ELEMENTS {
-    BOMB   = 1,
-    VOID   = 0,
+    BOMB = 1,
+    VOID = 0,
     VIEWED = -1
 };
 
-
-typedef int  Mine;
-typedef char Stroke;
-
+typedef int Mine;
 
 struct Problem {
-    unsigned int lineNumber;
-    unsigned int columnNumber;
-    unsigned int mineNumber;
-    Mine         *mineLoc;
+    unsigned lineNumber;
+    unsigned columnNumber;
+    unsigned mineNumber;
+    Mine *mineLoc;
 };
 
 struct Grille {
-    int **tab;
+    Problem pb;
+    unsigned **tab;
 };
 
 
-/**
- *
- */
-void createProblem();
-
-/**
- *
- * @param pb
- * @param mineList
- */
 void generateMine(Problem *pb, Mine *mineList);
+
+void defineProblem(Problem *pb, unsigned lines, unsigned column, unsigned mines);
+
+void generateGrid(Grille *gr);
+
+void defineProblem(Problem *pb, unsigned lines, unsigned column, unsigned mines) {
+    pb->lineNumber = lines;
+    pb->columnNumber = column;
+    pb->mineNumber = mines;
+    pb->mineLoc = new Mine[pb->mineNumber];
+}
 
 void generateMine(Problem *pb, Mine *mineList) {
     //TODO: Security about Problem
@@ -60,20 +58,73 @@ void generateMine(Problem *pb, Mine *mineList) {
     }
 }
 
+void setMines(Grille *gr) {
+    for(unsigned m = 0; m < gr->pb.mineNumber; m++) {
+        int mineLoc;
+        std::cin >> mineLoc;
+        gr->pb.mineLoc[m] = mineLoc;
+    }
+}
+
+void generateGrid(Grille *gr) {
+    gr->tab = new unsigned *[gr->pb.lineNumber];
+    for (int i = 0; i < gr->pb.lineNumber; i++) {
+        gr->tab[i] = new unsigned[gr->pb.columnNumber];
+    }
+}
+
+void printGrid(const Grille *g) {
+    unsigned lines = g->pb.lineNumber,
+            columns = g->pb.columnNumber;
+
+    std::cout << lines << " " << columns << std::endl;
+    for (unsigned int i = 0; i < lines; i++) {
+        for (unsigned _ = 0; _ < columns; _++) {
+            std::cout << " ___";
+        }
+        std::cout << std::endl;
+        for (unsigned int j = 0; j < columns; j++) {
+            //Todo, create function to show elemnt about his value
+            std::cout << ((j == 0) ? "| " : " ") << g->tab[i][j] << " |";
+        }
+        std::cout << std::endl;
+    }
+    for (unsigned _ = 0; _ < columns; _++) {
+        std::cout << " ___";
+    }
+}
+
+void fillGrid(Grille *g) {
+    unsigned lines = g->pb.lineNumber,
+            columns = g->pb.columnNumber;
+
+    for (unsigned int i = 0; i < lines; i++) {
+        for (unsigned int j = 0; j < columns; j++) {
+            unsigned readableNum = i * lines + j;
+            for(int m = 0; m < g->pb.mineNumber; m++) {
+                if(g->pb.mineLoc[m] == readableNum) {
+                    g->tab[i][j] = ELEMENTS::BOMB;
+                }
+            }
+            if(g->tab[i][j] != ELEMENTS::BOMB) g->tab[i][j] = ELEMENTS::VOID;
+        }
+    }
+}
+
 
 void createProblem() {
-    unsigned int line, column, mines;
-    Problem      problem {};
+    unsigned line, column, mines;
+    Problem problem{};
 
     std::cin >> line >> column >> mines;
 
     //Generate mines
-    problem.lineNumber   = line;
+    problem.lineNumber = line;
     problem.columnNumber = column;
-    problem.mineNumber   = mines;
-    problem.mineLoc      = new Mine[problem.mineNumber];
-
+    problem.mineNumber = mines;
+    problem.mineLoc = new Mine[problem.mineNumber];
     generateMine(&problem, problem.mineLoc);
+
 
     std::cout << problem.lineNumber << " " << problem.columnNumber << " " << problem.mineNumber << " ";
     for (int i = 0; i < problem.mineNumber; i++) {
@@ -87,21 +138,16 @@ void createProblem() {
 }
 
 void createGrille() {
-    //Todo à vérifier, car pas sûr.
-    //LIMITS
-    //LINE = LIMITS::LINES /:\ COLUMN = LIMITS::COLUMNS
-    unsigned int line = 7, column = 15;
-    Grille       gr {};
-    //Tab Init
-    gr.tab = new int *[line];
-    for (int i = 0; i < line; i++) {
-        gr.tab[i] = new int[column];
-    }
+    unsigned line, column, nbrMine;
+    Grille gr{};
 
-    //Now tab can be access with : tab[value][value];
-    //BOMB = ELEMENTS::BOMB
-    //VOID = ELEMENTS::VOID
-    //VIEWED = ELEMENTS::VIEWED
+    std::cin >> line >> column >> nbrMine;
+
+    defineProblem(&gr.pb, line, column, nbrMine);
+    generateGrid(&gr);
+    setMines(&gr);
+    fillGrid(&gr);
+    printGrid(&gr);
 
     //delete memory
     for (int i = 0; i < line; i++) {
@@ -115,7 +161,7 @@ int main() {
 
     srand(time(NULL));
 
-    int  cmd;
+    int cmd;
     bool loop = true;
 
     while (loop) {
