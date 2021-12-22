d7b7a2d0d711c50d86b6ad907fda8ec0bad469bd
BadiiiX <59808543+BadiiiiX@users.noreply.github.com>
2021-12-22 03:38:48 +0100

    ADD MANY THINGS :p
    - Refactor CASE Type
    + Enum STATE
    + Enum CONTENT
    - Edited Function :
        -- SetMines()
        -- ShowElement()
        + executeStroke()
        -- generateGrid()
        -- FillGrid()
        -- CreateGrille() [WIP]
        --

--- source.cpp
+++ source.cpp
@@ -16,14 +16,24 @@ enum LIMITS {
     LINES = 32
 };
 //tant que c'est différent de 0, on démasque la case mais pas les voisines, sinon si c'est égal à zéro, on démasque la case aux alentours.
-enum ELEMENTS {
-    VOID = 0,
-    BOMB = 10,
-    VIEWED = 20,
-    MARKED = 30
+enum STATE {
+    SHOWED = 1,
+    MARKED = 2,
+    HIDED = 0
 };
+
+enum CONTENT {
+    MINE = 10, //Here, 10 bc we cannot have more than 8.
+    VOID = 0
+};
+
 //type énuméré pour les lettres
-typedef int Mine;
+typedef unsigned Mine;
+
+struct Case {
+    unsigned content; //Can be CONTENT type
+    STATE state;
+};
 
 struct Stroke {
     char letter;
@@ -44,7 +54,7 @@ struct Problem {
 
 struct Grille {
     Problem pb;
-    unsigned **tab;
+    Case **tab;
     Historique histo;
 };
 
@@ -58,16 +68,9 @@ void generateGrid(Grille *gr);
 
 void setStroke(Grille &gr);
 
-void defineProblem(Problem *pb, unsigned lines, unsigned column, unsigned mines) {
-    pb->lineNumber = lines;
-    pb->columnNumber = column;
-    pb->mineNumber = mines;
-    pb->mineLoc = new Mine[pb->mineNumber];
-}
 
-void defineHisto(Historique *hist, unsigned nbrStroke) {
-    hist->nbrStrokes = nbrStroke;
-    hist->strokes = new Stroke[hist->nbrStrokes];
+bool validCase(Grille *gr, unsigned int x, unsigned int y) {
+    return (x >= 0 && x < gr->pb.lineNumber) && (y >= 0 && y < gr->pb.columnNumber);
 }
 
 void shuffleArray(Problem *p, unsigned int *tab) {
@@ -79,6 +82,19 @@ void shuffleArray(Problem *p, unsigned int *tab) {
         tab[j] = tab[i];
         tab[i] = ech;
     }
+}
+
+
+void defineProblem(Problem *pb, unsigned lines, unsigned column, unsigned mines) {
+    pb->lineNumber = lines;
+    pb->columnNumber = column;
+    pb->mineNumber = mines;
+    pb->mineLoc = new Mine[pb->mineNumber];
+}
+
+void defineHisto(Historique *hist, unsigned nbrStroke) {
+    hist->nbrStrokes = nbrStroke;
+    hist->strokes = new Stroke[hist->nbrStrokes];
 }
 
 // les nombres générés doivent être les bons : créer un tableau de taille 0 à n et l'initialiser, utiliser l'algo pour mélanger et prendre les n premières valeurs
@@ -101,27 +117,29 @@ void generateMine(Problem *pb, Mine *mineList) {
 
 void setMines(Grille *gr) {
     for (unsigned m = 0; m < gr->pb.mineNumber; m++) {
-        int mineLoc;
+        unsigned mineLoc;
+
         std::cin >> mineLoc;
         gr->pb.mineLoc[m] = mineLoc;
+
+        unsigned line = mineLoc / gr->pb.columnNumber,
+                column = mineLoc % gr->pb.columnNumber;
+
+        //Instant place mine.
+        if (validCase(gr, line, column)) gr->tab[line][column].content = CONTENT::MINE;
     }
 }
 
 void placeMines(Grille *g, unsigned i, unsigned j, unsigned numToCheck) {
     for (unsigned m = 0; m < g->pb.mineNumber; m++) {
         if (g->pb.mineLoc[m] == numToCheck) {
-            g->tab[i][j] = ELEMENTS::BOMB;
+            g->tab[i][j].content = CONTENT::MINE;
         }
     }
 }
-//fonction pour réveler les cases vides : 15 lignes et 20 lignes pour les autres.
-
-bool validCase(Grille *gr, unsigned int x, unsigned int y) {
-    return (x >= 0 && x < gr->pb.lineNumber) && (y >= 0 && y < gr->pb.columnNumber);
-}
 
 bool detectedMines(Grille *gr, unsigned int x, unsigned int y) { //adjacente : g/h : h/b : diagonale
-    return gr->tab[x][y] == ELEMENTS::BOMB;
+    return gr->tab[x][y].content == CONTENT::MINE;
 }
 
 /**
@@ -130,7 +148,7 @@ bool detectedMines(Grille *gr, unsigned int x, unsigned int y) { //adjacente : g
 *	@param[in] x, l'abscisse x de la grille
 *	@param[in] y, l'ordonnée y de la grille
 */
-unsigned int minesNearby(Grille *gr, unsigned int x, unsigned int y) {
+unsigned minesNearby(Grille *gr, unsigned int x, unsigned int y) {
     //Si c'est une mine, ça dégage ;)
     if (detectedMines(gr, x, y)) return 0;
 
@@ -160,17 +178,17 @@ unsigned int minesNearby(Grille *gr, unsigned int x, unsigned int y) {
     return mineCount;
 }
 
-char showElement(unsigned elementType) {
-    switch (elementType) {
-        case ELEMENTS::VIEWED:
-            return ' ';
-        case ELEMENTS::BOMB:
-        case ELEMENTS::VOID:
+char showElement(Case elementType) {
+    switch (elementType.state) {
+        case STATE::SHOWED:
+            if (elementType.content != CONTENT::MINE) return (char) (48 + elementType.content);
+            else return 'M';
+        case STATE::HIDED:
             return '.';
-        case ELEMENTS::MARKED:
+        case STATE::MARKED:
             return 'x';
         default:
-            return (char) elementType;
+            return (char) (48 + elementType.content);
     }
 }
 
@@ -186,20 +204,41 @@ char showElement(unsigned elementType) {
 //	}
 //}
 
-void setStroke(Grille &gr) {
-    for (unsigned int str = 0; str < gr.histo.nbrStrokes; str++) {
+void setStroke(Grille *gr) {
+    for (unsigned int str = 0; str < gr->histo.nbrStrokes; str++) {
         char stroke;
         unsigned int pos;
         std::cin >> stroke >> pos;
-        gr.histo.strokes[str].letter = stroke;
-        gr.histo.strokes[str].pos = pos;
+        gr->histo.strokes[str].letter = stroke;
+        gr->histo.strokes[str].pos = pos;
+    }
+}
+
+void executeStoke(Grille *gr) {
+    for (unsigned i = 0; i < gr->histo.nbrStrokes; i++) {
+        unsigned line = gr->histo.strokes[i].pos / gr->pb.columnNumber,
+                column = gr->histo.strokes[i].pos % gr->pb.columnNumber;
+
+        switch (gr->histo.strokes[i].letter) {
+            case 'M':
+                if (validCase(gr, line, column))
+                    gr->tab[line][column].state = MARKED;
+                break;
+            case 'D':
+                if (validCase(gr, line, column)) continue;
+                //FUNCTION TO DEMASK
+                break;
+            default:
+                //Throw Error ?
+                break;
+        }
     }
 }
 
 void generateGrid(Grille *gr) {
-    gr->tab = new unsigned *[gr->pb.lineNumber];
+    gr->tab = new Case *[gr->pb.lineNumber];
     for (unsigned i = 0; i < gr->pb.lineNumber; i++) {
-        gr->tab[i] = new unsigned[gr->pb.columnNumber];
+        gr->tab[i] = new Case[gr->pb.columnNumber];
     }
 }
 
@@ -234,8 +273,9 @@ void fillGrid(Grille *g) {
         for (unsigned j = 0; j < columns; j++) {
             unsigned readableNum = i * lines + j;
             placeMines(g, i, j, readableNum);
-            if (g->tab[i][j] != ELEMENTS::BOMB) g->tab[i][j] = ELEMENTS::VOID;
-            //g->tab[i][j] = minesNearby(g, i, j);
+            if (g->tab[i][j].content != CONTENT::MINE) g->tab[i][j].content = minesNearby(g, i, j);
+            g->tab[i][j].state = STATE::HIDED;
+            //
         }
     }
 }
@@ -275,9 +315,17 @@ void createGrille() {
     generateGrid(&gr);
     setMines(&gr);
 
-    //std::cin >> nbrStroke;
-
     fillGrid(&gr);
+
+    std::cin >> nbrStroke;
+    //TODO : STROKE SYSTEM
+
+    defineHisto(&gr.histo, nbrStroke);
+
+    setStroke(&gr);
+
+    executeStoke(&gr);
+
     printGrid(&gr);
 
     //delete memory
@@ -285,6 +333,8 @@ void createGrille() {
         delete[] gr.tab[i];
     }
     delete[] gr.tab;
+    delete[] gr.histo.strokes;
+    delete[] gr.pb.mineLoc;
 }
 
 int main() {
@@ -295,8 +345,9 @@ int main() {
     bool loop = true;
 
     while (loop) {
+        //WARNING: If cmd get a string, loop can be infinite ;(
         std::cin >> cmd;
-        switch (cmd) {
+        switch ((int) cmd) {
             case 1:
                 createProblem();
                 break;
@@ -318,8 +369,8 @@ int main() {
                 loop = false;
 
             default:
-                std::cin.ignore(17, '\n');
-                std::cout << "Commande inconnue" << std::endl;
+                std::cout << "Commande inconnue";
+                break;
 
         }
     }
