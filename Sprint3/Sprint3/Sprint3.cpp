0afb70f12dd3d47a7b76d926d185ac35aa18ad5a
Beowulf-0 <73909294+Beowulf-0@users.noreply.github.com>
2022-01-04 21:23:48 +0100

    Update Sprint3 / Sprint4

--- Sprint3/Sprint3/Sprint3.cpp
+++ Sprint3/Sprint3/Sprint3.cpp
@@ -19,35 +19,29 @@ int main() {
 	int cmd;
 	bool loop = true;
 
-	while (loop) {
-		//WARNING: If cmd get a string, loop can be infinite ;(
-		std::cin >> cmd;
-		switch ((int)cmd) {
-		case 1:
-			createProblem();
-			break;
+	std::cin >> cmd;
+	switch ((int)cmd) {
+	case 1:
+		createProblem();
+		break;
 
-		case 2:
-			createGrille();
-			break;
+	case 2:
+		createGrille();
+		break;
 
-		case 3:
-			gameWon();
-			break;
+	case 3:
+		gameWon();
+		break;
 
-		case 4:
-			break;
+	case 4:
+		break;
 
-		case 5:
-			break;
+	case 5:
+		break;
 
-		case 6:
-			loop = false;
+	default:
+		std::cout << "Commande inconnue";
+		break;
 
-		default:
-			std::cout << "Commande inconnue";
-			break;
-
-		}
 	}
 }
